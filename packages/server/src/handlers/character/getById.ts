06a589e67f70b91e4375499f8bf9987d26db339b
Arttu Pennanen <arttu@pennanen.org>
2022-02-02 01:00:12 +0200

    refactor(server): use .get in getById

--- packages/server/src/handlers/character/getById.ts
+++ packages/server/src/handlers/character/getById.ts
@@ -2,14 +2,14 @@ import { Character, Handler } from '../../types/global';
 
 export const getById: Handler<number, Character | null | Error> = (ctx, id) => {
   return new Promise((resolve, reject) => {
-    return ctx.globals.db.all(
+    return ctx.globals.db.get(
       'SELECT * FROM character WHERE id = ?',
       [id],
       (err, result) => {
         if (err) {
           reject(err);
         }
-        resolve(result[0] || null);
+        resolve(result || null);
       },
     );
   });
